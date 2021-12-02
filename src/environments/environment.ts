838c25682934c21c196e8a64b483ce2dce282c42
Sunny Pek <sunny1996@live.com.sg>
2021-12-02 14:55:42 +0800

    added environment url for api calls

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  tokenApiUrl: "http://localhost:3000/",
+  zendeskApiUrl: "https://zccsunnypek.zendesk.com/api/v2/tickets",
 };
 
 /*
