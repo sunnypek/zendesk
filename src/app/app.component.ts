29f87996e7c7275c2232c55a3c4036574abf8bab
Sunny Pek <sunny1996@live.com.sg>
2021-12-02 10:43:16 +0800

    added logic for changing theme and storing theme in localstorage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from "@angular/cdk/layout";
+import { OverlayContainer } from "@angular/cdk/overlay";
+import { Component } from "@angular/core";
+import { Observable } from "rxjs";
+import { map, shareReplay } from "rxjs/operators";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'zendesk';
+  title = "zendesk";
+  currTheme: string = localStorage.getItem("theme") ? <string>localStorage.getItem("theme") : "indigo-pink-theme";
+
+  constructor(private overlayContainer: OverlayContainer, private breakpointObserver: BreakpointObserver) {}
+
+  ngOnInit(): void {
+    this.overlayContainer
+      .getContainerElement()
+      .classList.add(localStorage.getItem("theme") ? <string>localStorage.getItem("theme") : "indigo-pink-theme");
+  }
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+    map((result) => result.matches),
+    shareReplay()
+  );
+
+  changeTheme(newTheme: string): void {
+    let oldTheme: string = <string>localStorage.getItem("theme");
+    this.currTheme = newTheme;
+    localStorage.setItem("theme", newTheme);
+    this.overlayContainer.getContainerElement().classList.remove(oldTheme);
+    this.overlayContainer.getContainerElement().classList.add(newTheme);
+  }
 }
