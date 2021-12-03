e5c143ccf2e19cef0490cf1646b7529e5f343111
Sunny Pek <sunny1996@live.com.sg>
2021-12-03 20:31:59 +0800

    added service method to get single ticket

--- src/app/services/zendesk.service.ts
+++ src/app/services/zendesk.service.ts
@@ -20,6 +20,15 @@ export class ZendeskService {
     );
   }
 
+  getTicket(id: string): Observable<Ticket> {
+    return this.http.get<Ticket>(environment.tokenApiUrl + id).pipe(
+      catchError((error: HttpErrorResponse) => {
+        this.handleError(error);
+        return throwError(error.statusText);
+      })
+    );
+  }
+
   private handleError(error: HttpErrorResponse): void {
     if (error.status === 0) {
       // A client-side or network error occurred. Handle it accordingly.
