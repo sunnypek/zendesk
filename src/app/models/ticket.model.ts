a4eaf59baf5d903dda8bbf210a987c347cb6eb86
Sunny Pek <sunny1996@live.com.sg>
2021-12-03 17:33:33 +0800

    update ticket model with correct property

--- src/app/models/ticket.model.ts
+++ src/app/models/ticket.model.ts
@@ -1,7 +1,7 @@
 export class Ticket {
   constructor(
     public id: string,
-    public desc: string,
+    public description: string,
     public url: string,
     public subject: string,
     public priority: string,
