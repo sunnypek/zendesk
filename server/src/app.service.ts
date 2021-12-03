60d07affd010f56d80335e75fe9ceac9539bf0eb
Sunny Pek <sunny1996@live.com.sg>
2021-12-03 16:59:22 +0800

    get data from zendesk API with error handling

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -1,10 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { catchError, map, Observable } from 'rxjs';
+import { Ticket } from './model/ticket.model';
+import { ZendeskReponse } from './model/zendeskResponse.model';
 
 @Injectable()
 export class AppService {
-  getEncodedToken(): string {
-    return Buffer.from(process.env.EMAIL + ':' + process.env.PASS).toString(
-      'base64',
-    );
+  constructor(private httpService: HttpService) {}
+
+  private encodedToken: string = Buffer.from(
+    process.env.EMAIL + ':' + process.env.PASS,
+  ).toString('base64');
+
+  getTickets(): Observable<Ticket[]> {
+    return this.httpService
+      .get<ZendeskReponse>(process.env.APIURL, {
+        headers: { Authorization: 'Basic ' + this.encodedToken },
+      })
+      .pipe(
+        map((response) => response.data.tickets),
+        catchError((error: HttpException) => {
+          throw new HttpException(
+            'API not reachable',
+            HttpStatus.SERVICE_UNAVAILABLE,
+          );
+        }),
+      );
   }
 }
