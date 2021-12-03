b8317c1dd7843c9aad476a553019c6cd910654ea
Sunny Pek <sunny1996@live.com.sg>
2021-12-03 17:00:13 +0800

    added zendesk service in frontend

--- src/app/services/zendesk.service.ts
+++ src/app/services/zendesk.service.ts
@@ -1,5 +1,9 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Observable, throwError } from "rxjs";
+import { catchError, map } from "rxjs/operators";
+import { Ticket } from "server/dist/model/ticket.model";
+import { environment } from "../../environments/environment";
 
 @Injectable({
   providedIn: "root",
@@ -7,7 +11,21 @@ import { Injectable } from "@angular/core";
 export class ZendeskService {
   constructor(private http: HttpClient) {}
 
-  getTickets(): any {
-    return;
+  getTickets(): Observable<Ticket[]> {
+    return this.http.get<Ticket[]>(environment.tokenApiUrl).pipe(
+      catchError((error: HttpErrorResponse) => {
+        this.handleError(error);
+        return throwError(error.statusText);
+      })
+    );
+  }
+
+  private handleError(error: HttpErrorResponse): void {
+    if (error.status === 0) {
+      // A client-side or network error occurred. Handle it accordingly.
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+    }
   }
 }
